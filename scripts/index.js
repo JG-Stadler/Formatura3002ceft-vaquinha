ed5630f78d4582ff013e1b43d5080289455698a4
João Stadler <joaogbstadler@gmail.com>
2024-05-01 13:05:24 -0300

    Caixa de apresentação de fotos adicionada

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+// Mostruario de imagens
+var pictures =[
+    "renata.jpg","igulinho.jpg","joão.jpg","raycon.jpg",
+    "robin.jpg","davi.jpg","pedro.jpg","vitinho.jpg","lala.jpg",
+    "duzão.jpg","julia.jpg","luiz.jpg","carol.jpg","cadu.jpg",
+    "sarah.g.jpg","sarah.f.jpg","enzzo.jpg","samuel.jpg"
+]
+var moldura = document.getElementById("foto-aluno");
+
+let i = 0;
+const ChangeImage= setInterval(()=>{
+     if(i===17){
+         i=0;
+     }
+     moldura.src = `images/${pictures[i]}`;
+     i++;
+    },1500);
